d4dfb47e9ef64c090fce13a1f24ade8996694f9b
TarikB <tarikbdursun@gmail.com>
2024-08-14 18:17:13 +0300

    +Initial Commit : Rust Actix-Web Backend Project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpServer};
+
+mod routes;
+use routes::*;
+
+#[tokio::main]
+async fn main() ->std::io::Result<()> {
+    let server=HttpServer::new(|| App::new()
+        .service(home)
+        .service(hello_user)
+        .service(create_new_user))
+        .bind(("127.0.0.1",8000))?
+        .run();
+
+    println!("Server Running At 127.0.0.1:8000");
+    server.await
+}
