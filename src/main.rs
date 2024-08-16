0ef327d4e870db2c605641243740cf749a8a3648
TarikB <tarikbdursun@gmail.com>
2024-08-16 21:45:59 +0300

    +Added : MySql Connection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use actix_web::{App, HttpServer};
+use actix_web::{http::Error, App, HttpServer};
 
 mod routes;
 use routes::*;
 
-#[tokio::main]
-async fn main() ->std::io::Result<()> {
-    let server=HttpServer::new(|| App::new()
+mod database;
+use database::*;
+
+#[tokio::main] //Result<Pool<MySql>, Error>
+async fn main() ->std::io::Result<()> 
+{
+    let database = database_connection()
+        .await
+        .expect("Failed to connect to database");
+
+    println!("Database connection established");
+
+    let server=HttpServer::new(move || 
+    App::new()
+        .app_data(database.clone())
         .service(home)
         .service(hello_user)
         .service(create_new_user))
